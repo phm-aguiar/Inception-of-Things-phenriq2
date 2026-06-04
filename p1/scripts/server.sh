f6e2d6a98f256a5fd2bbc6fc02150f7bdd8dbdf2
cauemendess <cauemendess@users.noreply.github.com>
2026-06-04 15:26:29 -0300

    refactor: remove unnecessary no-sudo kubectl config from server scripts

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -8,9 +8,3 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
 
 cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
 
-# kubectl no sudo permission
-mkdir -p /home/vagrant/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sudo chown vagrant:vagrant /home/vagrant/.kube/config
-echo 'export KUBECONFIG=/home/vagrant/.kube/config' >> /home/vagrant/.bashrc
-echo 'export PATH=$PATH:/sbin' >> /home/vagrant/.bashrc
